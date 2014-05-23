ca4b47a9711c0fd96557113b4f913f0d620803ce
cernigls <cernigls@gmail.com>
2014-05-23 09:29:18 -0400

    Updated comments of stub functions
    Added full descriptions of overall description and individual functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion is a costly computation.  These functions implement
+## a solution to cache a special "matrix" and retrieve it if its inverse
+## has already been calculated before.  The special "matrix" is actually
+## a list containing a function to: 
+##  1) set the value of the matrix
+##  2) get the value of the matrix
+##  3) set the value of the inverse of the matrix
+##  4) get the value of the inverse of the matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the speical "matrix" returned by
+## makeCacheMatrix function.  If the inverse has already been calculated
+## then the cacheSolve will retrieve the inverse from the cache, otherwise
+## it will calcuated it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
